6d55846e9bd33d8d916887e15ace2af8cc1955a1
KailasChandran <kailaskochery@gmail.com>
2026-04-08 10:57:37 +0530

    Coded UC4: Display Hello with Multiple Command-Line Arguments

--- src/Helloapp.java
+++ src/Helloapp.java
@@ -4,9 +4,23 @@ public class HelloApp {
         // Default name
         String name = "World";
 
-        // Check if argument is provided
+        // Check if arguments are provided
         if (args.length > 0) {
-            name = args[0];
+
+            // Use StringBuilder to combine multiple names
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+
+                // Add comma only between names
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+
+            // Assign combined names
+            name = nameBuilder.toString();
         }
 
         // Display output
